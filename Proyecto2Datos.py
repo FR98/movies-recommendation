f2acbab9e5fe7093c6abcce7fba2cdc0f315f17d
Francisco Rosal <francisco-rosal98@hotmail.com>
2019-05-02 20:27:27 -0600

    Avances

--- Proyecto2Datos.py
+++ Proyecto2Datos.py
@@ -1,6 +1,11 @@
-from neo4j import GraphDatabase
+# Proyecto 2 Estructuras de Datos
+# Gian Luca Rivera 1804
+# Francisco Rosal 18676
+# Programa de recomendacion de peliculas
 
-class HelloWorldExample(object):
+from neo4j.v1 import GraphDatabase, basic_auth
+
+class DataBase(object):
 
     def __init__(self, uri, user, password):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -19,3 +24,23 @@ class HelloWorldExample(object):
                         "SET a.message = $message "
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
+
+
+# ----------------------------------------------
+
+# driver = GraphDatabase.driver(
+#     "bolt://52.91.116.38:38229",
+#     auth=basic_auth("neo4j", "frame-partners-staplers"))
+# session = driver.session()
+#
+# cypher_query = '''
+# MATCH (n)
+# RETURN id(n) AS id
+# LIMIT $limit
+# '''
+#
+# results = session.run(cypher_query,
+#   parameters={"limit": 10})
+#
+# for record in results:
+#   print(record['id'])
